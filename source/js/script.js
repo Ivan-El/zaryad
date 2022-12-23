b426f7b9c4fbaff5e2bee0338f4897f06e23db43
Иван <lostnemo@rambler.ru>
2022-12-23 14:01:13 +0600

    Добавляет стилизацию слайдеров

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,39 @@
-//
+const feedbackCircles = document.getElementsByClassName(
+  "feedback-slider__feedback"
+);
+
+const initCircleTypes = () => {
+  for (const circle of feedbackCircles) {
+    new CircleType(circle);
+  }
+};
+
+const feedbackSlider = new Swiper(".feedback-slider", {
+  loop: true,
+  slidesPerView: "auto",
+  loopedSlides: 3,
+  centeredSlides: true,
+  spaceBetween: 201,
+  navigation: {
+    nextEl: ".feedback-slider__button-next",
+    prevEl: ".feedback-slider__button-prev",
+  },
+});
+
+feedbackSlider.on("slideChangeTransitionStart", () => {
+  initCircleTypes();
+});
+
+
+const wareSlider = new Swiper(".ware-slider", {
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".ware-slider__button-next",
+    prevEl: ".ware-slider__button-prev",
+  },
+});
+
+initCircleTypes();
+
